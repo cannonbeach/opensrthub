2e700852d4d373b601497565019b9bb1b72d3ac2
cannonbeach <cannonbeachgoonie@gmail.com>
2023-12-05 15:30:08 -0800

    Set NodeJS version to 18

--- setupopensrthub.sh
+++ setupopensrthub.sh
@@ -98,7 +98,7 @@ echo "srthub: making keyrings directory /etc/apt/keyrings for Nodesource GPG key
 sudo mkdir -p /etc/apt/keyrings
 echo "srthub: importing Nodesource GPG key"
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=12
+NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 echo "srthub: performing global Ubuntu update (again)"
 sudo apt-get update -y
