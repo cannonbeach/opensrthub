b7dcfa1c6fd26487986415774ca2e1b3cd3e3090
cannonbeach <cannonbeachgoonie@gmail.com>
2023-12-11 15:51:06 -0800

    Disabled verbose mode for curl signal posts

--- source/esignal.c
+++ source/esignal.c
@@ -114,7 +114,7 @@ void signal_management_interface(srthub_core_struct *core, char *signal_buffer, 
         }
 
         curl_easy_setopt(curl, CURLOPT_URL, signal_url);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, (char*)signal_buffer);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)signal_buffer_length);
